25cf67c7769bf39c4b3cc8d61901e59e93aac984
Danil Nurgaliev <nurgalievdanil.ios@gmail.com>
2022-11-08 20:53:02 +0300

    Complete palindrome number task

--- Easy/Palindrome Number.playground/Contents.swift
+++ Easy/Palindrome Number.playground/Contents.swift
@@ -1,1 +1,14 @@
+/*
+ Given an integer x, return true if x is a palindrome, and false otherwise.
+ 
+ Input: x = 121
+ Output: true
+ Explanation: 121 reads as 121 from left to right and from right to left.
+ */
 
+class Solution {
+    func isPalindrome(_ x: Int) -> Bool {
+        let xString = String(x)
+        return xString == String(xString.reversed())
+    }
+}
